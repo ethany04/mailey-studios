6a5c6674ac5a4a19dbffe369e1065ec582564ad1
ethany04 <ethan.yu@utexas.edu>
2025-05-07 22:37:31 -0500

    imported assets

--- mailey/src/App.jsx
+++ mailey/src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const App = () => {
-  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <h1>
+            Find <span class="text-gradient">Movies</span> You'll Enjoy Without
+            the Hassle
+          </h1>
+        </header>
+
+        <p>Search</p>
+      </div>
+    </main>
+  );
 };
 
 export default App;
