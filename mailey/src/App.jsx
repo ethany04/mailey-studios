484151ffafc17a9c75172143f538d66cee7577cc
ethany04 <ethan.yu@utexas.edu>
2025-05-07 22:27:21 -0500

    added tailwind css

--- mailey/src/App.jsx
+++ mailey/src/App.jsx
@@ -1,34 +1,7 @@
-import { useEffect, useState } from "react";
-
-const Card = ({ title }) => {
-  const [count, setCount] = useState(0);
-  const [hasLiked, setHasLiked] = useState(false);
-
-  useEffect(() => {
-    console.log(`${title} has been liked: ${hasLiked}`);
-  }, [hasLiked]);
-
-  return (
-    <div className="card" onClick={() => setCount(count + 1)}>
-      <h2>
-        {title} <br /> {count || null}
-      </h2>
-
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? "❤️" : "🤍"}
-      </button>
-    </div>
-  );
-};
+import React from "react";
 
 const App = () => {
-  return (
-    <div className="card-container">
-      <Card title="Star Wars" />
-      <Card title="Avatar" />
-      <Card title="THe Lion King" />
-    </div>
-  );
+  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
 };
 
 export default App;
